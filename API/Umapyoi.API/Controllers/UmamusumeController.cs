31a98b9d48b002e0938c2279dc5026728ead2127
Ewerton Shoiti Okada <105746487+ewerton139@users.noreply.github.com>
2026-06-15 22:26:27 -0300

    Add files via upload

---
+++ API/Umapyoi.API/Controllers/UmamusumeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Umapyoi.API.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class UmamusumeController : ControllerBase
+    {
+        [HttpGet]
+
+    }
+}
